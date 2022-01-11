f2ba3fedd79ca79d1faffa05165232badc49690d
Shane Cowherd <shane@cowherd.com>
2022-01-11 11:42:41 -0700

    Send hostname every second

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const os = require('os');
 const express = require('express');
 const path = require('path');
 const { createServer } = require('http');
@@ -11,16 +11,23 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
+const hostname = os.hostname();
 
 wss.on('connection', function (ws) {
   const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignore errors.
-      //
-    });
-  }, 100);
+    ws.send(hostname);
+//     ws.send(JSON.stringify(process.memoryUsage()), function () {
+//       //
+//       // Ignore errors.
+//       //
+//     });
+  }, 1000);
   console.log('started client interval');
+  
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//     ws.send(hostname);
+//   });
 
   ws.on('close', function () {
     console.log('stopping client interval');
